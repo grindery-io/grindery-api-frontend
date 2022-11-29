be5f092baa70840e9650fc206919227ddedfa258
Egor Miliukov <milukove@gmail.com>
2022-11-29 04:09:06 +0200

    add early access modal

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext } from "react";
+import NexusClient from "grindery-nexus-client";
+import React, { createContext, useEffect, useState } from "react";
 import { useGrinderyNexus } from "use-grindery-nexus";
 import { defaultFunc } from "../helpers/utils";
 
@@ -7,6 +8,9 @@ type ContextProps = {
   user: string | null;
   disconnect: any;
   connect: any;
+  accessAllowed: boolean;
+  verifying: boolean;
+  client: NexusClient | null;
 };
 
 // Context provider props
@@ -19,11 +23,43 @@ export const AppContext = createContext<ContextProps>({
   user: null,
   disconnect: defaultFunc,
   connect: defaultFunc,
+  accessAllowed: false,
+  verifying: true,
+  client: null,
 });
 
 export const AppContextProvider = ({ children }: AppContextProps) => {
   // Auth hook
   const { user, connect, disconnect } = useGrinderyNexus();
+
+  const [accessAllowed, setAccessAllowed] = useState<boolean>(false);
+
+  // verification state
+  const [verifying, setVerifying] = useState<boolean>(true);
+
+  // Nexus API client
+  const [client, setClient] = useState<NexusClient | null>(null);
+
+  const verifyUser = async () => {
+    setVerifying(true);
+    const res = await client?.isAllowedUser().catch((err) => {
+      console.error("isAllowedUser error:", err.message);
+      setAccessAllowed(false);
+    });
+    if (res) {
+      setAccessAllowed(true);
+    } else {
+      setAccessAllowed(false);
+    }
+    setVerifying(false);
+  };
+
+  useEffect(() => {
+    if (user) {
+      verifyUser();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
 
   return (
     <AppContext.Provider
@@ -31,6 +67,9 @@ export const AppContextProvider = ({ children }: AppContextProps) => {
         user,
         disconnect,
         connect,
+        accessAllowed,
+        verifying,
+        client,
       }}
     >
       {children}
